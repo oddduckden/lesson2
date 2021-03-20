886e64d3cc3fc73265e065749d16b8f997b3e638
oddduckden <dkireev.71@gmail.com>
2021-03-20 19:45:43 +0300

    убогая реализация task2

--- task2.py
+++ task2.py
@@ -2,7 +2,36 @@
 # 2 и 3 и т.д. При нечетном количестве элементов последний сохранить на своем месте. Для заполнения списка элементов
 # необходимо использовать функцию input().
 
+# test string: 1,eee, r, (1, 2), azaza, {A, B, C},[2, 2, 5]
+
 source_string = input('Введите через запятую элементы списока: ')
-while source_string:
-    source_string = input('Введите через запятую элементы списока: ')
-source_list = list(source_string.split(', '))
+while not source_string:
+    source_string = input('Введите через запятую элементы списка: ')
+# парсинг введенной строки в список
+source_string = source_string.replace(' ', '')
+skip_it = False
+converted_string = str()
+for i in source_string:
+    if i in ('[', '{', '('):
+        skip_it = True
+    elif i in (']', '}', ')'):
+        skip_it = False
+    if i == ',' and not skip_it:
+        converted_string = converted_string + ';'
+    elif i == ',' and skip_it:
+        converted_string = converted_string + ', '
+    else:
+        converted_string = converted_string + i
+source_list = list(converted_string.split(';'))
+# попарная перестановка с учетом нечетного
+result = list()
+last_element = str()
+if len(source_list) % 2 != 0:
+    last_element = source_list.pop(-1)
+for i, j in zip(*[iter(source_list)]*2):
+    i, j = j, i
+    result.append(i)
+    result.append(j)
+if last_element:
+    result.append(last_element)
+print(', '.join(result))
